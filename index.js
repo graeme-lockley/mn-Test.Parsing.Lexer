c3ca1b692ec1bf69adde537695f259410559412b
Graeme Lockley <graemel@no9.co.za>
2017-05-10 07:07:30 +0200

    refactor: curried all of the internal functions and changed the style from a const to a function style

--- index.js
+++ index.js
@@ -9,18 +9,6 @@ function LexerState(configuration, state) {
 }
 
 
-function mkRunningState(input, index, position, token, oldPosition, oldIndex) {
-    return {
-        input: input,
-        index: index,
-        position: position,
-        token: token,
-        oldPosition: oldPosition,
-        oldIndex: oldIndex
-    };
-}
-
-
 LexerState.prototype.token = function () {
     return this.state.token;
 };
@@ -42,11 +30,11 @@ LexerState.prototype.next = function () {
     } else {
         const currentState = skipWhitespaceComments(this.configuration)(this.state);
         if (isEndOfFile(currentState.index)(currentState.input)) {
-            return new LexerState(this.configuration, finalState(this.configuration, currentState));
+            return new LexerState(this.configuration, finalState(this.configuration)(currentState));
         } else {
-            const errorState = advanceState(currentState, currentState.input[currentState.index], this.configuration.err(currentState.input[currentState.index]));
+            const errorState = advanceState(currentState)(currentState.input[currentState.index])(this.configuration.err(currentState.input[currentState.index]));
             const mapTokenPattern = tokenPattern =>
-                tokenPattern.first.matchFrom(currentState.input)(currentState.index).map(text => advanceState(currentState, text, tokenPattern.second(text)));
+                tokenPattern.first.matchFrom(currentState.input)(currentState.index).map(text => advanceState(currentState)(text)(tokenPattern.second(text)));
 
             return new LexerState(this.configuration, this.configuration.tokenPatterns.findMap(mapTokenPattern).withDefault(errorState));
         }
@@ -59,49 +47,54 @@ LexerState.prototype.eof = function () {
 };
 
 
-function isEndOfFile(index) {
-    return input => index >= input.length;
-}
+const isEndOfFile = index => input =>
+    (index >= input.length);
 
 
-function skipWhitespaceComments(configuration) {
-    return state => {
-        function findComment() {
-            return configuration.comments.findMap(comment => comment.open.matchFrom(state.input)(state.index).map(_ => comment));
-        }
+const skipWhitespaceComments = configuration => state => {
+    const findComment = () =>
+        configuration.comments.findMap(comment => comment.open.matchFrom(state.input)(state.index).map(_ => comment));
 
-        function applyComment(comment) {
-            let index = state.index + 1;
+    const applyComment = comment => {
+        let index = state.index + 1;
 
-            while (!isEndOfFile(index)(state.input)) {
-                const closeMatch = comment.close.matchFrom(state.input)(index);
+        while (!isEndOfFile(index)(state.input)) {
+            const closeMatch = comment.close.matchFrom(state.input)(index);
 
-                if (closeMatch.isJust()) {
-                    const commentString = state.input.substr(state.index, closeMatch.withDefault("").length + index - state.index);
-                    return advanceState(state, commentString);
-                } else {
-                    index += 1;
-                }
+            if (closeMatch.isJust()) {
+                const commentString = state.input.substr(state.index, closeMatch.withDefault("").length + index - state.index);
+                return advanceState(state)(commentString)(configuration.eof);
+            } else {
+                index += 1;
             }
-
-            return advanceState(state, state.input.substr(state.index));
         }
 
+        return advanceState(state)(state.input.substr(state.index))(configuration.eof);
+    };
 
-        const possibleComment = findComment();
+    const possibleComment = findComment();
 
-        return possibleComment.map(comment => skipWhitespaceComments(configuration)(applyComment(comment)))
-            .withDefault(
-                configuration
-                    .whitespacePattern
-                    .andThen(whitespacePattern => whitespacePattern.matchFrom(state.input)(state.index))
-                    .map(matchedText => skipWhitespaceComments(configuration)(advanceState(state, matchedText)))
-                    .withDefault(state));
-    }
-}
+    return possibleComment.map(comment => skipWhitespaceComments(configuration)(applyComment(comment)))
+        .withDefault(
+            configuration
+                .whitespacePattern
+                .andThen(whitespacePattern => whitespacePattern.matchFrom(state.input)(state.index))
+                .map(matchedText => skipWhitespaceComments(configuration)(advanceState(state)(matchedText)(configuration.eof)))
+                .withDefault(state));
+};
 
 
-function advanceState(currentState, matchedText, matchedToken) {
+const mkRunningState = input => index => position => token => oldPosition => oldIndex => ({
+    input: input,
+    index: index,
+    position: position,
+    token: token,
+    oldPosition: oldPosition,
+    oldIndex: oldIndex
+});
+
+
+const advanceState = currentState => matchedText => matchedToken => {
     const advancedIndex = currentState.index + matchedText.length;
     const advancePositionOnCharacter = position => item =>
         item.charCodeAt(0) === 10
@@ -109,46 +102,23 @@ function advanceState(currentState, matchedText, matchedToken) {
             : position.mapFirst(x => x + 1);
     const advancedPosition = String.foldl(matchedText)(currentState.position)(advancePositionOnCharacter);
 
-    return mkRunningState(
-        currentState.input,
-        advancedIndex,
-        advancedPosition,
-        matchedToken,
-        currentState.position,
-        currentState.index);
-}
+    return mkRunningState(currentState.input)(advancedIndex)(advancedPosition)(matchedToken)(currentState.position)(currentState.index);
+};
 
 
-function initialState(input) {
-    return mkRunningState(
-        input,
-        0,
-        Tuple(1)(1),
-        undefined,
-        Tuple(1)(1),
-        0);
-}
+const initialState = input =>
+    mkRunningState(input)(0)(Tuple(1)(1))(undefined)(Tuple(1)(1))(0);
 
 
-function finalState(configuration, state) {
-    return mkRunningState(
-        state.input,
-        state.input.length,
-        state.position,
-        configuration.eof,
-        state.position,
-        state.input.length
-    );
-}
+const finalState = configuration => state =>
+    mkRunningState(state.input)(state.input.length)(state.position)(configuration.eof)(state.position)(state.input.length);
 
 
-function setup(configuration) {
-    return {
-        fromString: function (input) {
-            return new LexerState(configuration, initialState(input)).next();
-        }
-    };
-}
+const setup = configuration => ( {
+    fromString: function (input) {
+        return new LexerState(configuration, initialState(input)).next();
+    }
+});
 
 
 module.exports = {
